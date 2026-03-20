29bf343909a476467c8fb2c032a9f6d03fa46b4c
andreysuslov167-max <andreysuslov167@gmail.com>
2026-03-20 16:45:53 +0530

    Add files via upload

---
+++ teeee/test_work/target/debug/deps/test_work-a096e6353043969d.d
@@ -0,0 +1,5 @@
+/home/andrey/test_work/target/debug/deps/test_work-a096e6353043969d.d: src/main.rs
+
+/home/andrey/test_work/target/debug/deps/libtest_work-a096e6353043969d.rmeta: src/main.rs
+
+src/main.rs:
